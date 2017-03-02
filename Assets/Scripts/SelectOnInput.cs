305c4662378b12975200bc1b92d0f7892e04a8ef
Sheila <Sheila@Sheilas-MacBook-Pro.local>
2017-03-01 19:17:01 -0700

    Main Menu
    Added a main menu with Start, Audio, Help & Quick features.

---
+++ Assets/Scripts/SelectOnInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectOnInput : MonoBehaviour {
+
+	public UnityEventQueueSystem eventSystem;
+	public GameObject selectedObject;
+
+	private bool buttonSelected;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetAxisRax ("Vertical") ! = 0 && buttonSelected == false)
+			{
+			eventSystem.SetSelectedGameObject(selectedObject);
+			buttonSelected = true;
+			}
+	}
+
+	private void OnDisable ()
+	{
+		buttonSelected = false;
+	}
+}
