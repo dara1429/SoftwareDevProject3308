7609c20d042118cc948010b3f248e3e977d16a9b
justinhxc <jubr5704@colorado.edu>
2017-03-05 13:59:59 -0700

    background animation
    -have the background shifting between blue and purple.
    -code written to shift circle from red to yellow, but does not work yet.
    -next step after this is edititng the buttons.

---
+++ Assets/shiftColorBlot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//animate the backround colors to shift between multiple different colors
+public class shiftColorBlot : MonoBehaviour
+{
+
+    public Color colOne = new Color(121, 219, 82);
+    public Color colTwo = Color.red;
+
+    public float dura = 5.0F;
+    Image image;
+
+    // Use this for initialization
+    void Start()
+    {
+        image = GetComponent<Image>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float length = Mathf.PingPong(Time.time, dura) / dura;
+        image.color = Color.Lerp(colOne, colTwo, length);
+    }
+}
---
+++ Assets/shiftColors.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//animate the backround colors to shift between multiple different colors
+public class shiftColors : MonoBehaviour {
+
+    public Color colOne = new Color(190,74,178);
+    public Color colTwo = Color.blue;
+
+    public float dura = 4.0F;
+
+    Camera cam;
+	// Use this for initialization
+    void Start () {
+        cam = GetComponent<Camera>();
+        cam.clearFlags = CameraClearFlags.SolidColor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float length = Mathf.PingPong(Time.time, dura) / dura;
+        cam.backgroundColor = Color.Lerp(colOne, colTwo, length);
+    }
+}
