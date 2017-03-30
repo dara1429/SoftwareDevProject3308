5b6436857b250456c1e2b9c1d2c01ce4f4d602ee
justinhxc <jubr5704@colorado.edu>
2017-03-30 16:41:42 -0600

    Menu updated
    -Changed to flashing icon and note buttons
    -got colors and menus smoothed
    -shifting colors works (background makes it look like graffti

---
+++ Assets/ColorShiftBG.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorShiftBG : MonoBehaviour {
+
+    private Color colOne = Color.yellow;//yellow   
+    private Color colTwo = Color.blue;//blue
+    private float dura = 2.0F;//time between shifting intervals
+    SpriteRenderer image;//used to grab the blot image
+
+    void Start()//gets the camera component to apply changes on
+    {
+        image = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        //shifts the colors as needed
+        float length = Mathf.PingPong(Time.time, dura) / dura;
+        image.color = Color.Lerp(colOne, colTwo, length);
+    }
+}
---
+++ Assets/colorChangePanels.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorChangePanels : MonoBehaviour {
+
+    private Color colOne = Color.yellow;//yellow   
+    private Color colTwo = Color.blue;//blue
+    private float dura = 2.0F;//time between shifting intervals
+    //Image image;//used to grab the image
+
+    void Start()//gets the camera component to apply changes on
+    {
+        //image = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        //shifts the colors as needed
+        //float length = Mathf.PingPong(Time.time, dura) / dura;
+        //image.color = Color.Lerp(colOne, colTwo, length);
+    }
+}
