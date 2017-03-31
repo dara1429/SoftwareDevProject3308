437af52d6a2e7601adefbac05e7b0361ec62888b
justinhxc <jubr5704@colorado.edu>
2017-03-31 16:22:56 -0600

    working on sounds/music
    have the sliders almost set. Working on code now.

---
+++ Assets/menuManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class menuManager : MonoBehaviour {
+
+    public GameObject audioMenu;
+    public Slider[] volumeSliders;
+
+	public void masterVolume(float val)
+    {
+        AudioManager.instance.SetVolume (val, AudioManager.AudioChannel.Master);
+    }
+
+    public void musicVolume(float val)
+    {
+        AudioManager.instance.SetVolume(val, AudioManager.AudioChannel.Music);
+    }
+
+    public void sfxVolume(float val)
+    {
+        AudioManager.instance.SetVolume(val, AudioManager.AudioChannel.Sfx);
+    }
+}
