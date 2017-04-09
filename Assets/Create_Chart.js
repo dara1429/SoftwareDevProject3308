414873984d6d32460a7fab8af48a7432eff1b8b4
dara1429 <dara1429@colorado.edu>
2017-04-08 21:41:16 -0600

    fe73e8c + chart creator progress
    - includes audio fix commit
    - basic chart creator code complete. creates a txt file (custom.txt) and
    writes inputs from the user (1,2,3,4) into the file while the song
    (custom.mp3) plays.

--- Assets/Create_Chart.js
+++ Assets/Create_Chart.js
@@ -3,34 +3,53 @@ import System.IO;
 //This file will create a new chart (text) file in Assets/Music/ 
 
 function Start () {
-	if (File.Exists("Assets/Music/example2.txt"))
+	if (File.Exists("Assets/Music/custom.txt"))
 	{
 	Debug.Log("Chart already exists.");
+	//Display Error Message and exit to main menu.
 	//Application.LoadLevel("MainMenu");
 	}
-	var chart = new StreamWriter("Assets/Music/example2.txt");
-	chart.WriteLine("Test");
+
+	var song : AudioSource = GetComponent.<AudioSource>(); 
+  	var time : int = Mathf.Round(song.clip.length);
+	var timing : int = time * 16;
+	chartNotes(timing);
+}
+
+function chartNotes (timing : int) 
+{
+	var counter : int = 0;
+	var chart = new StreamWriter("Assets/Music/custom.txt");
+	while(counter < timing)
+	{
+		if (Input.GetButtonDown ("1"))
+		{
+	 		chart.WriteLine("1000");
+		}
+		if (Input.GetButtonDown ("2"))
+		{
+			chart.WriteLine("0100");
+		}	
+		if (Input.GetButtonDown ("3"))
+		{
+			chart.WriteLine("0010");
+		}	
+		if (Input.GetButtonDown ("4"))
+		{
+			chart.WriteLine("0001");
+		}
+		else
+		{
+			if (counter != 0)
+			{
+			chart.WriteLine("0000");
+			}
+		}
+		yield WaitForSeconds(.0625);
+		counter = counter + 1;
+	}
 	chart.Close();
-
+	yield WaitForSeconds(2);
+	Application.LoadLevel("MainMenu");
 }
 
-function Update () {
-
-	if (Input.GetButtonDown ("1"))
-	{
-	 
-	}
-	if (Input.GetButtonDown ("2"))
-	{
-
-	}	
-	if (Input.GetButtonDown ("3"))
-	{
-
-	}	
-	if (Input.GetButtonDown ("4"))
-	{
-
-	}
-
-}
--- Assets/Pause Menu.js
+++ Assets/Pause Menu.js
@@ -6,18 +6,21 @@ private var paused: boolean = false;
 
 function Update(){
     //Looks for Input P to toggle pause, pausing when unpaused and unpaussing when paused
+    var song : AudioSource = GetComponent.<AudioSource>(); 
     if (Input.GetKeyDown(KeyCode.P)){
         if (paused){
             Time.timeScale = 1;
             paused = false;
         }
         else {
+        	song.Pause();
             Time.timeScale = 0;
             paused = true;
         }
     //Same as above, but this one triggers if one presses the button to continue
     }
     if (!paused){
+    	song.UnPause();
         Time.timeScale = 1;
     }
     else {
